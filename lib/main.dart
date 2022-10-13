148572b9009338f70c51b42becee2414398fc010
Fara <115084566+KHALMATOV-FARRUKH@users.noreply.github.com>
2022-10-13 19:42:11 +0500

    The first commit, creating the compass application

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // First of all let's set the Compass for the app
+  double? heading = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FlutterCompass.events!.listen((event) {
+      setState(() {
+      heading = event.heading;
+
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade900,
+        centerTitle: true,
+        title: const Text("Compass App"),
+      ),
+
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "${heading!.ceil()}°",
+            style: const TextStyle(
+                color: Colors.white,
+                fontSize: 26.0,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 50.0,),
+          // Now let's show the compass
+          Padding(padding: EdgeInsets.all(18.0),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset("assets/images/cadrant.png"),
+                Transform.rotate(angle: ((heading ?? 0) * (pi / 180) * -1),
+                child: Image.asset("assets/images/compass.png", scale: 1.1,
+                ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
